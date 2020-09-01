8521e1f53114476949258145b42a4f359f6b3a25
Ripa-Shah <70597931+Ripa-Shah@users.noreply.github.com>
2020-09-01 12:30:30 -0700

    Create advisee role logic.sql

---
+++ advisee role logic.sql
@@ -0,0 +1,10 @@
+select distinct(sch.ID_NUM),tw.group_id,nm.FIRST_NAME,nm.LAST_NAME from TW_GRP_MEMBERSHIP tw
+left outer join STUDENT_CRS_HIST sch on sch.ID_NUM=tw.ID_NUM
+left outer join NAME_MASTER nm on nm.ID_NUM=sch.ID_NUM
+where sch.ID_NUM not in (select t.id_num from TW_GRP_MEMBERSHIP t left outer join STUDENT_CRS_HIST s on s.ID_NUM=t.ID_NUM where t.group_id 
+in('student','admiss_admin','admiss_coun','admiss_officer','advisor','advisor_admin',
+'alumni','candidate','dev_officer','employ_applcnt','faculty','finaid_admin','general_ledger',
+'guest','staff','staff_admin','student','student_admin'))
+
+--select * from TW_GROUP;
+
